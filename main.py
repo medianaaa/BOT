6f1b472a1cb412d15fd49fd9aeba1e395c1a13df
medianaaa <diana.repich09@gmail.com>
2022-02-27 15:43:58 +0300

    first commit

--- main.py
+++ main.py
@@ -56,5 +56,6 @@ def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
 
 
-bot.polling(none_stop=True)
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
 
